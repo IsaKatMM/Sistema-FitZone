555106c607bb4b2f03e817d21c6fab9656157c1e
IsaKatMM <isabel.morocho@unl.edu.ec>
2025-10-15 23:21:22 -0500

    Estructura inicial de proyecto Spring Boot

---
+++ sistema-FitSIL/src/main/java/sistema_FitSIL/Application.java
@@ -0,0 +1,14 @@
+package sistema_FitSIL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+//aqui se levanta la app
